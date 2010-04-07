6f013dde4441650c73cf7dbd47330736638e521e
John Trammell <johntrammell@gmail.com>
2010-04-07 12:23:33 -0500

    playing with layout

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Intro
+=====
+
+This package is an extension of setuptools' egg_info command.  Where the
+egg_info command is unwisely specific to the Subversion version control
+system, the gitegginfo command is unwisely and explicitly tied to the Git
+DVCS (distributed version control system).
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gitegginfo',
+    version='0.1',
+    description="Setuptools extension to use Git version information",
+    long_description=open('readme.txt').read() + '\n\n' + \
+        open('history.txt').read() + '\n',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='setuptools egg_info git',
+    author='John Trammell',
+    author_email='bills@holmescorp.com',
+    url='http://www.holmescorp.com',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points = {
+        "distutils.commands": [
+            "gitegginfo = gitegginfo:git_egg_info",
+        ],
+    },
+)
