8ed0f719941d7049853dbeecc291049159ff20bb
John Trammell <johntrammell@gmail.com>
2011-07-08 10:27:55 -0500

    more exposition on revisions

--- lib/gitegginfo/revision.py
+++ lib/gitegginfo/revision.py
@@ -1,26 +1,38 @@
 """
-gitegginfo
+Commands to getting git revision strings (from either git, or from the
+underlying svn repository if using git-svn).
 
-Create a distribution's .egg-info directory and contents, pulling version
-information from git or "git svn" if possible.
+Also, found on http://peak.telecommunity.com/DevCenter/setuptools ::
+
+    A post-release tag is either a series of letters that are alphabetically
+    greater than or equal to "final", or a dash (-). Post-release tags are
+    generally used to separate patch numbers, port numbers, build numbers,
+    revision numbers, or date stamps from the release number. For example, the
+    version 2.4-r1263 might denote Subversion revision 1263 of a post-release
+    patch of version 2.4. Or you might use 2.4-20051127 to denote a
+    date-stamped post-release.
+
+The prefix "r-" is used to denote subversion postreleases, and the prefix "g-"
+is used for git postreleases.
 """
 
 from subprocess import Popen, PIPE
 from setuptools.command.egg_info import get_pkg_info_revision
 
 
-def get_git_description(self):
+def get_git_description():
     """
-    Try to pull a suitable revision number from the output of 'git describe'.
-    The output will look like <tag>-<commits>-<obj>, e.g.  "v1.0-14-g2414721";
-    to massage it into something that can be used, the following code strips
-    off the tag portion of the description.
+    Use the output of 'git describe' as our revision number.  The output will
+    look like <tag>-<commits>-<obj>, e.g. "1.0-14-g24a41ec".
     """
+    # At some point when I understand the use cases better, may need to do some
+    # manipulation of the description string to make a better version ID...
     try:
+        #import pdb; pdb.set_trace()
         p = Popen(['git', 'describe', '--always'], stdout=PIPE, stderr=PIPE)
         p.stderr.close()
-        rev = p.stdout.readlines()[0].strip().split('-')
-        return "-".join(rev[1:])
+        desc = p.stdout.readlines()[0].strip()
+        return desc
     except:
         return None
 
